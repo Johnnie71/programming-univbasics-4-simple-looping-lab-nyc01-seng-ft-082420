cfba16a7931b9125aab770d4f8dfb3ae386cef5b
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2020-08-01 20:14:40 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,10 +1,10 @@
 # Write your methods here
 
-def loop_message_five_times("string")
+def loop_message_five_times(string)
   counter = 0
   
   while counter < 5 do
-    puts "string"[counter]
+    puts string[counter]
     counter += 1
   end
 end
