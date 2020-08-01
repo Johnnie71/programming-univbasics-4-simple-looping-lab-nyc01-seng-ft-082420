3c6770e3bc4a80bc7c1baa48c83a4fce883458f5
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2020-08-01 20:31:15 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -26,3 +26,13 @@ def output_array(array)
     counter += 1 
   end
 end
+
+def return_string_array(array)
+  counter = 0 
+  new_array = []
+  
+  while counter < array.length do
+    new_array.push(array[count].to_s)
+    array[counter].to_s
+    counter += 1 
+end 
